50e2ac8395f2f0740c3ec250e2cdead3d5fc1206
Weizhen Wang <wangwzhen@gmail.com>
2019-08-14 09:54:54 -0500

    update for heroku

---
+++ build/precache-manifest.1e29ce367c8c481af01edceacafbe03f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14de5b7c9efc0c98070c75fdd4db19a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a40c432bbe40c2e02d4",
+    "url": "/static/css/main.03c12d56.chunk.css"
+  },
+  {
+    "revision": "6d9eb2286d6d2f876ef0",
+    "url": "/static/js/2.0d9fc34e.chunk.js"
+  },
+  {
+    "revision": "8a40c432bbe40c2e02d4",
+    "url": "/static/js/main.0cb3c9ab.chunk.js"
+  },
+  {
+    "revision": "1c17a606a51b501ef344",
+    "url": "/static/js/runtime~main.b20c73a6.js"
+  }
+]);
